82718025338e436ca37e09923b055aab21b600e0
gnadigpeter <xlurzx@gmail.com>
2022-02-07 19:09:33 +0100

    67

--- airport/src/main/java/hu/webuni/airport/service/FlightSpecifications.java
+++ airport/src/main/java/hu/webuni/airport/service/FlightSpecifications.java
@@ -5,26 +5,33 @@ import java.time.LocalTime;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import hu.webuni.airport.model.Airport_;
 import hu.webuni.airport.model.Flight;
+import hu.webuni.airport.model.Flight_;
 
 
 public class FlightSpecifications {
 	public static Specification<Flight> hasId(long id){
-		//return (root, cq, cb) -> cb.equal(root.get(Flight_.id), id);
-		return (root, cq, cb) -> cb.equal(root.get("id"), id);
+		return (root, cq, cb) -> cb.equal(root.get(Flight_.id), id);
+		//return (root, cq, cb) -> cb.equal(root.get("id"), id);
 	}
 
 	public static Specification<Flight> hasFlightNumber(String flightNumber) {
-		return (root, cq, cb) -> cb.like(root.get("flightNumber"), flightNumber+"%");
+		return (root, cq, cb) -> cb.like(root.get(Flight_.flightNumber), flightNumber+"%");
+		//return (root, cq, cb) -> cb.like(root.get("flightNumber"), flightNumber+"%");
 	}
 	
 	public static Specification<Flight> hasTakeoffTime(LocalDateTime takeoffTime) {
 		LocalDateTime startOfDay = LocalDateTime.of(takeoffTime.toLocalDate(), LocalTime.of(0, 0));
-		return (root, cq, cb) -> cb.between(root.get("takeoffTime"), 
+//		return (root, cq, cb) -> cb.between(root.get("takeoffTime"), 
+//				startOfDay, startOfDay.plusDays(1));
+		return (root, cq, cb) -> cb.between(root.get(Flight_.takeoffTime), 
 				startOfDay, startOfDay.plusDays(1));
 	}
 	public static Specification<Flight> hasTakeoffIata(String takeoffIata) {
-		return (root, cq, cb) -> cb.like(root.get("takeoff").get("iata"),
+//		return (root, cq, cb) -> cb.like(root.get("takeoff").get("iata"),
+//				takeoffIata+"%");
+		return (root, cq, cb) -> cb.like(root.get(Flight_.takeoff).get(Airport_.IATA),
 				takeoffIata+"%");
 	}
 }
